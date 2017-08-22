ade1fe2f783f7da4dd81be90b9d3a1aa7ded0360
hellloamman <amansharma33.as@gmail.com>
2017-08-22 19:45:13 +0530

    carrers

---
+++ index.php
@@ -0,0 +1,389 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <title>Career</title>
+
+  
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#6f2774">
+    <link rel="shortcut icon" type="image/png" href="img/logo.png"/>
+
+   
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/superslides.css"> 
+    <link rel="stylesheet" href="css/elastic_grid.css"> 
+    <link rel='stylesheet prefetch' href='https://cdn.rawgit.com/pguso/jquery-plugin-circliful/master/css/jquery.circliful.css'>    
+    <link href="css/slick.css" rel="stylesheet"> 
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/animate.css"> 
+    <link id="orginal" href="css/themes/default-theme.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+	<!-- Google fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Varela' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+
+  </head>
+  <style type="text/css">
+    
+    hr {
+  border-color: #3F51B5;
+  border-width: 3px;
+  max-width: 50px;
+}
+hr.light {
+  border-color: white;
+}
+
+element.style {
+}
+.main-section.paddind {
+    padding-bottom: 60px;
+}
+
+.button {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+  </style>
+  <body> 
+
+    <!-- SCROLL TOP BUTTON -->
+    <a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
+    <!-- END SCROLL TOP BUTTON -->
+
+    <!--=========== BEGIN HEADER SECTION ================-->
+    <header id="header">
+      <!-- BEGIN MENU -->
+      <div class="menu_area">
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation"> 
+          <div class="container">
+          <div class="navbar-header">
+            <!-- FOR MOBILE VIEW COLLAPSED BUTTON -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+
+            <!-- LOGO -->
+
+            <!-- TEXT BASED LOGO -->
+            <img style="padding-left:20px;" class="img-responsive" src="img/logo8.png">
+            
+            
+            <!-- IMG BASED LOGO  -->
+            <!--  <a class="navbar-brand" href="#"><img src="img/logo.png" alt="logo"></a> --> 
+            
+                   
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+
+            <ul id="top-menu" class="nav navbar-nav navbar-right main_nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#ourvision">our vision</a></li>
+             
+              <li><a href="#Department">Department</a></li> 
+
+              <li><a href="#contact">Contacts</a></li>                  
+            </ul>           
+          </div><!--/.nav-collapse -->
+          </div>     
+        </nav>  
+      </div>
+      <!-- END MENU -->
+
+      <!-- BEGIN SLIDER AREA-->
+      <div class="slider_area">
+        <!-- BEGIN SLIDER-->          
+        <div id="slides">
+          <ul class="slides-container">
+
+            <!-- THE FIRST SLIDE-->
+            <li>
+              <!-- FIRST SLIDE OVERLAY -->
+              <div class="slider_overlay"></div> 
+              <!-- FIRST SLIDE MAIN IMAGE -->
+              <img src="img/sliders/slide.jpg" alt="img">
+              <!-- FIRST SLIDE CAPTION-->
+              <div class="slider_caption">
+                <h2>Careers at <br>SPI Cinemas</h2>  
+              </div>
+            </li>
+
+            <!-- THE SECOND SLIDE-->
+            <li>
+              <!-- SECOND SLIDE OVERLAY -->
+              <div class="slider_overlay"></div> 
+              <!-- SECOND SLIDE MAIN IMAGE -->
+              <img src="img/sliders/Slide2.jpg" alt="img">
+              <!-- SECOND SLIDE CAPTION-->
+              <div class="slider_caption">
+                <h2><h2>Careers at <br>SPI Cinemas</h2></h2>
+              </div>
+            </li>
+
+            <!-- THE THIRD SLIDE-->
+            <li>
+              <!-- THIRD SLIDE OVERLAY -->
+              <div class="slider_overlay"></div> 
+              <!-- THIRD SLIDE MAIN IMAGE -->
+              <img src="img/sliders/Slide3.jpg" alt="img">
+              <!-- THIRD SLIDE CAPTION-->
+              <div class="slider_caption">
+                <h2><h2>Careers at <br>SPI Cinemas</h2></h2>
+              </div>
+            </li>
+			
+          </ul>
+          <!-- BEGAIN SLIDER NAVIGATION -->
+          <nav class="slides-navigation">
+            <!-- PREV IN THE SLIDE -->
+            <a class="prev" href="/item1">
+              <span class="icon-wrap"></span>
+              <h3><strong>Prev</strong></h3>
+            </a>
+            <!-- NEXT IN THE SLIDE -->
+            <a class="next" href="/item3">
+              <span class="icon-wrap"></span>
+              <h3><strong>Next</strong></h3>
+            </a>
+          </nav>       
+        </div>
+        <!-- END SLIDER-->          
+      </div>
+      <!-- END SLIDER AREA -->
+    </header>
+    <!--=========== End HEADER SECTION ================-->
+
+
+    
+	
+
+    <!--=========== BEGIN ABOUT SECTION ================-->
+    <section id="ourvision" style="background-color:#6f2774;">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 col-md-12">
+            <div class="about_area">
+              <!-- START ABOUT HEADING -->
+              <div class="heading">
+                <h2 class="wow fadeInLeftBig" style="color:white;">Our Vision</h2>
+              </div>
+
+              <!-- START ABOUT CONTENT -->
+              <div class="about_content">
+                <div class="row">
+                  <div class="col-lg-6 col-md-6 col-sm-12">
+                    <div class="about_featured">
+                   <div class="featured_slider">
+                        <!-- SINGLE SLIDE IN THE SLIDER -->
+                        <div class="single_iteam">
+                        <h3 style="color: white">Lorem Ipsum</h3>
+                        <p style="color: white">
+                          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.    </p>                      
+                        </div>
+                        <!-- SINGLE SLIDE IN THE SLIDER -->
+                        <div class="single_iteam">
+                          <h3 style="color: white">Lorem iugud nIpsum</h3>
+                        <p style="color: white">
+                          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.    </p>                  
+                        </div>
+                        <!-- SINGLE SLIDE IN THE SLIDER -->
+                        <div class="single_iteam">
+                          <h3 style="color: white">Lorem ff Ipsum</h3>
+                        <p style="color: white">
+                          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.    </p>    
+                        </div>
+                        <!-- SINGLE SLIDE IN THE SLIDER -->
+                        <div class="single_iteam">
+                          <h3 style="color: white"> Ipsum</h3>
+                        <p style="color: white">
+                          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.    </p>    
+                        </div>
+                        <!-- SINGLE SLIDE IN THE SLIDER -->
+                        <div class="single_iteam">
+                         <h3 style="color: white">Lorem Ipjhbjvsum</h3>
+                        <p style="color: white">
+                          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.    </p>    
+                        </div>
+                      </div>
+                      <!-- END FEATURED SLIDER -->
+                    </div>
+                  </div>
+                  <div class="col-lg-6 col-md-6 col-sm-12">
+                    <div class="about_slider">
+                      <!-- BEGAIN FEATURED SLIDER -->
+                      <div class="featured_slider">
+                        <!-- SINGLE SLIDE IN THE SLIDER -->
+                        <div class="single_iteam">
+                          <a href="#"><img src="img/feature_img1.jpeg" alt="img"></a>                          
+                        </div>
+                        <!-- SINGLE SLIDE IN THE SLIDER -->
+                        <div class="single_iteam">
+                          <a href="#"><img src="img/feature_img2.jpeg" alt="img"></a>                          
+                        </div>
+                        <!-- SINGLE SLIDE IN THE SLIDER -->
+                        <div class="single_iteam">
+                          <a href="#"><img src="img/feature_img3.jpeg" alt="img"></a>                           
+                        </div>
+                        <!-- SINGLE SLIDE IN THE SLIDER -->
+                        <div class="single_iteam">
+                          <a href="#"> <img src="img/feature_img4.jpeg" alt="img"></a>                           
+                        </div>
+                        <!-- SINGLE SLIDE IN THE SLIDER -->
+                        <div class="single_iteam">
+                          <a href="#"> <img src="img/feature_img5.png" alt="img"></a>                           
+                        </div>
+                      </div>
+                      <!-- END FEATURED SLIDER -->
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>       
+      </div>
+               <!-- END SKILLS  -->
+    </section>
+    <!--=========== END ABOUT SECTION ================-->
+
+    <!--===========Begian with Department Section============-->
+   
+
+    <!--=========== BEGAIN CONTACT SECTION ================-->
+    <section id="contact" style="background-color:white;">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 col-md-12 col-sm-12">
+            <!-- START CONTACT HEADING -->
+            <div class="heading">
+              <h2 class="wow fadeInLeftBig">Contact Us</h2>
+              <p>We are always up and working. Send us mail or give us a call, we will get back to you for sure!! Or simply give us a visit at the location shown in the map.</p>
+            </div>
+          </div>
+        </div>       
+        <div class="row">
+          <!-- BEGAIN CONTACT CONTENT -->
+          <div class="contact_content">
+            <!-- BEGAIN CONTACT FORM -->
+          
+            <!-- BEGAIN CONTACT MAP -->
+            <div class="col-lg-12 col-md-12 col-sm-12">
+              <div class="contact_map">
+              <!-- BEGAIN GOOGLE MAP -->
+               <div id="map_canvas"></div>
+              </div>
+            </div>           
+          </div>
+        </div>      
+      </div>             
+    </section>
+    <!--=========== END CONTACT SECTION ================-->
+
+    <!--=========== BEGAIN CONTACT FEATURE SECTION ================-->
+    <section id="contactFeature">
+      <!-- SKILLS COUNTER OVERLAY -->
+      <div class="slider_overlay"></div>
+      <div class="row">
+        <div class="col-lg-12 col-md-12">       
+          <div class="container">               
+              <div class="contact_feature">
+            <!-- BEGAIN CALL US FEATURE -->
+            <div class="col-lg-4 col-md-4 col-sm-6">
+              <div class="single_contact_feaured wow fadeInUp">
+                <i class="fa fa-phone"></i>
+                <h4>Call Us</h4>
+                <center><hr style="padding:5px;"></center>
+                <strong><p style="color:white;">asdfghj : 9962438445</p> </strong>
+                <strong><p style="color:white;">asdfghjk : 9962551950</p>       </strong>
+              </div>
+            </div>
+            <!-- BEGAIN CALL US FEATURE -->
+            <div class="col-lg-4 col-md-4 col-sm-6">
+              <div class="single_contact_feaured wow fadeInUp">
+                <i class="fa fa-envelope-o"></i>
+                <h4>Email Address</h4>
+                <center><hr></center>
+                <strong><p style="color:white;">asdfghj@gmail.com</p></strong><br><br>
+              </div>
+            </div>
+            <!-- BEGAIN CALL US FEATURE -->
+            <div class="col-lg-4 col-md-4 col-sm-6">
+              <div class="single_contact_feaured wow fadeInUp">
+                <i class="fa fa-map-marker"></i>
+                <h4>Location</h4>
+                <center><hr></center>
+                <strong><p style="color:white;">asdfghjkl, asdfghjklt, asdfghjkl,<br> asdfghjk, Kattankulathur</p></strong><br>
+              </div>
+            </div>
+          </div>
+          </div>         
+        </div>
+      </div>
+    </section>
+
+     <footer id="footer" style="background-color:#6f2774;">
+       <div class="container">
+         <div class="row">
+           <div class="col-lg-6 col-md-6 col-sm-6">
+             <div class="footer_left">
+				<!--=========== Designed By WpFreeware Team ================-->
+               <p>Connect with us on Social Media</p>
+			   <!--=========== Designed By WpFreeware Team ================-->
+             </div>
+           </div>
+           <div class="col-lg-6 col-md-6 col-sm-6">
+             <div class="footer_right">
+               <ul class="social_nav">
+                 <li><a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a></li>
+                 
+               </ul>
+             </div>
+           </div>
+         </div>
+       </div>
+      </footer>-->
+      <!--=========== END FOOTER ================-->
+
+     <!-- Javascript Files
+     ================================================== -->
+  
+     <!-- initialize jQuery Library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Google map -->
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="js/jquery.ui.map.js"></script>
+     <!-- For smooth animatin  -->
+    <script src="js/wow.min.js"></script> 
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- superslides slider -->
+    <script src="js/jquery.superslides.min.js" type="text/javascript"></script>
+    <!-- slick slider -->
+    <script src="js/slick.min.js"></script>    
+    <!-- for circle counter -->
+    <script src='https://cdn.rawgit.com/pguso/jquery-plugin-circliful/master/js/jquery.circliful.min.js'></script>
+    <!-- for portfolio filter gallery -->
+    <script src="js/modernizr.custom.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/elastic_grid.min.js"></script>
+    <script src="js/portfolio_slider.js"></script>
+
+    <!-- Custom js-->
+    <script src="js/custom.js"></script>
+  </body>
+</html>
